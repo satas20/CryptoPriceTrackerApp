a89f9656a9cf6cc5ba5020fc160d898cc3d91986
Ata Ayyıldız <84992313+satas20@users.noreply.github.com>
2024-03-24 14:45:22 +0300

    Using API instead

---
+++ src/main/java/com/example/cyrptoapp/CyrptoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.cyrptoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyrptoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyrptoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cyrptoapp/CyrptoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cyrptoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyrptoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
